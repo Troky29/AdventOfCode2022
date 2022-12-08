f898c12bc1971b7c1b72b8ba258a851522d019f7
Troky29 <lorenzorosinitroky@live.it>
2022-12-08 17:23:11 +0100

    Fixed formatting and typo

--- day03.py
+++ day03.py
@@ -1,15 +1,8 @@
 import itertools
 from pathlib import Path
 from typing import List
-
 from utils import read_lines
 
-TEST = """vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw"""
 
 def parse_input(input_lines: List[str]) -> List[List[str]]:
     rucksacks = []
@@ -36,7 +29,8 @@ def get_misplaced_items(rucksacks: List[List[str]]) -> List[str]:
 
 
 def get_items_priority(misplaced_items: List[str]) -> List[int]:
-    priority = {chr(key): value + 1 for (value, key) in enumerate(itertools.chain(range(ord('a'), ord('z')+1), range(ord('A'), ord('Z')+1)))}
+    priority = {chr(key): value + 1 for (value, key) in
+                enumerate(itertools.chain(range(ord('a'), ord('z') + 1), range(ord('A'), ord('Z') + 1)))}
     misplaced_items_priority = []
     for misplaced_item in misplaced_items:
         misplaced_items_priority.append(priority[misplaced_item])
@@ -47,7 +41,7 @@ def get_items_priority(misplaced_items: List[str]) -> List[int]:
 def get_missing_badges(rucksacks: List[List[str]]) -> List[str]:
     missing_badges = []
     for i in range(0, len(rucksacks), 3):
-        missing_badge = set(rucksacks[i]).intersection(set(rucksacks[i+1]).intersection(set(rucksacks[i+2])))
+        missing_badge = set(rucksacks[i]).intersection(set(rucksacks[i + 1]).intersection(set(rucksacks[i + 2])))
         missing_badges.append(missing_badge.pop())
 
     return missing_badges
@@ -59,12 +53,11 @@ if __name__ == "__main__":
     puzzle_lines = read_lines(puzzle_part1_path)
     puzzle_input = parse_input(puzzle_lines)
 
-    misaplece_items = get_misplaced_items(puzzle_input)
-    misplaced_items_priority = get_items_priority(misaplece_items)
+    misplaced_items = get_misplaced_items(puzzle_input)
+    misplaced_items_priority = get_items_priority(misplaced_items)
 
     missing_badges = get_missing_badges(puzzle_input)
     missing_badges_priority = get_items_priority(missing_badges)
 
     print(f"Part 1 solution: {sum(misplaced_items_priority)}")
     print(f"Part 2 solution: {sum(missing_badges_priority)}")
-
